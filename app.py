c6a7aa74e697e92143a70fe2a50afa2598ee2b62
AkshayKumarIITG <akshay.kumar3389@gmail.com>
2020-04-20 11:49:30 +0530

    greeting resp added

--- app.py
+++ app.py
@@ -7,22 +7,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+	return "Hello, World!"
 
 @app.route("/sms", methods=['POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
-    phone_no = request.form.get('From')
+def reply():
+   
+	incoming_msg = request.values.get('Body', '')
+	#print(incoming_msg)
+	resp = MessagingResponse()
+	msg = resp.message()
+	responded = False
 
-    reply = fetch_reply(msg, phone_no)
+	if 'Hi' in incoming_msg or 'Hey' in incoming_msg:
+		text = f'Hey 👋, \nThis is a Covid-Bot developed by Akshay to provide latest information updates i.e cases in different countries and create awareness to help you and your family stay safe. \n Please enter one of the following option 👇 \n *A*. Covid-19 statistics *Worldwide*. \n *B*. Covid-19 cases in *India*. \n *C*. Covid-19 cases in *China*. \n *D*. Covid-19 cases in *USA*. \n *E*. Coronavirus cases in *Italy*. \n *F*. How does it *Spread*? \n *G*. *Preventive measures* to be taken.'
+		msg.body(text)
+		responded = True
 
-    # Create reply
-    resp = MessagingResponse()
-    resp.message(reply)
-
-    return str(resp)
-
+	return str(resp)
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
